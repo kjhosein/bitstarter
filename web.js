7d624a6b00ec54090a974e2dd4146785e466686f
Khalid J Hosein <kjh@pobox.com>
2013-07-13 14:48:05 -0400

    Updated to read from index.html and use buffers.

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
+console.log('inputtext is:' + buf); //debug
+
+var output = buf.toString();
+console.log('output is:' + output); //debug
+//var testv = 'this is testv';
+//console.log("this is testv:" + testv);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  //response.send('Hello World 3!' + testv);
+  //response.send('Hello World 3! ' + output);
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
